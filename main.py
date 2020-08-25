88c914ba28953d56e610ae98a61aa0ca8bbaf7d1
Mindcool225 <thezachm24@gmail.com>
2020-08-25 22:16:33 +0000

    Renamed simple to basic, fixed indentation errors.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_prefix(bot, message):
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
 
-extensions = ['cogs.simple']
+extensions = ['cogs.basic']
 
 bot = commands.Bot(
     command_prefix=get_prefix, description='A bot for general use.')
