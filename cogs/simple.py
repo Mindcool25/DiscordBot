b944b39cd748deefeb8c6c4bed87469201041934
Mindcool225 <thezachm24@gmail.com>
2020-08-25 18:44:54 +0000

    Updated cogs.simple

---
+++ cogs/simple.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from datetime import datetime as d
+
+class MembersCog(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command(
+		name="ping",
+		description="Check bot speed and connection",
+		aliases=['p']
+		)
+	async def ping_command(self,ctx):
+		start = d.timestamp(d.now())
+		msg = await ctx.send(content='Pinging')
+		await msg.edit(content=f'Pong!\nOne message round-trip took {( d.timestamp( d.now() ) - start ) * 1000 }ms.')
+		return
+	@commands.command(
+		name="quote",
+		description="Get a random quote from the wonderful dnd group."
+		aliases=
+	)
+
+def setup(bot):
+	bot.add_cog(MembersCog(bot))
